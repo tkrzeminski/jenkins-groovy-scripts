03b6fc1966798f0bf502ccf1f83527fa47a8ea58
Microscotch <github.com@microscotch.net>
2022-12-07 18:38:05 +0100

    Add ability to retrieve specifics credentials
    * Use method with a variable number of arguments to get a single entry point to retrieve all credentials or specific ones (see See https://docs.groovy-lang.org/docs/groovy-2.4.12/html/documentation/#_varargs)
    * Factorization of credential rendering

--- show-all-credentials.groovy
+++ show-all-credentials.groovy
@@ -1,40 +1,64 @@
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
 
-def showRow = { credentialType, domain, secretId, username = null, password = null, description = null ->
-  println("${credentialType} : ".padLeft(20) + (domain?:'global')?.padRight(20) + "|" + secretId?.padRight(38)+" | " +username?.padRight(20)+" | " +password?.padRight(40) + " | " +description)
+// When called without paramters will returns all global credentials
+retrieveCredentials()
+// To retrieve specific credentials, just pass their ids as parameter.
+// For instance to retrieve credentials credId_1 and credId_2, just perform
+// such call:
+// retrieveCredentials('credId_1','credId_2')
+  
+def retrieveCredentials(String... credIds) {
+  
+  def credentialsProvider = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0]
+  credentialsProvider.domainCredentials.each { domainCredentials ->
+    credentialsProvider.getCredentials(domainCredentials.domain).findAll{ credential ->
+      credIds.size() == 0 || credential.id in credIds
+    }.each { credential ->
+      displayCredential(domainCredentials.domain.name,credential)
+    }
+  }
+  
+  // Prevent any returns on console script
+  null
+  
 }
 
-def credentialsProvider = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0]
-credentialsProvider.domainCredentials.each { domainCredentials ->
-  credentialsProvider.getCredentials(domainCredentials.domain).each {
-  	switch(it.class.name) {
-      case "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl":
-        showRow("user/password", domainCredentials.domain.name, it.id, it?.username, it?.password?.plainText, it.description)
-        break
-      case "com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey":
-        showRow("ssh priv key", domainCredentials.domain.name, it.id, it.privateKeySource?.privateKey?.plainText, it.passphrase?.decrypt()?:"", it.description)
-        break
-      case "com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl":
-        showRow("aws", domainCredentials.domain.name, it.id, it.accessKey, it.secretKey?.plainText, it.description)
-        break
-      case "org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl":
-        showRow("secret text", domainCredentials.domain.name, it.id, it.secret?.plainText, '', it.description)
-        break
-      case "org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl":
-        showRow("secret file", domainCredentials.domain.name, it.id, it.content?.text, '', it.description)
-        break
-      case "com.microsoft.azure.util.AzureCredentials":
-        showRow("azur", domainCredentials.domain.name, it.id, it?.subscriptionId, "${it?.clientId}:${hudson.util.Secret.decrypt(it?.clientSecret)}", it.description)
-        break
-      case "org.jenkinsci.plugins.docker.commons.credentials.DockerServerCredentials":      
-        showRow("docker", domainCredentials.domain.name, it.id, it.clientCertificate, it?.clientKey, it.description)
-        break
-      case "com.dabsquared.gitlabjenkins.connection.GitLabApiTokenImpl":
-        showRow("gitlab", domainCredentials.domain.name, it.id, it.apiToken?.plainText, '' , it.description)
-        break      
-      default:
-        showRow("something else", domainCredentials.domain.name, it.id, it.class.name, '', it.description)
-  	}         
+def displayCredential(def domain, def cred) {
+  // Closure in charge to display credentials details
+  def showRow = { credentialType, domainName, secretId, username = null, password = null, description = null ->
+    println("${credentialType} : ".padLeft(20) + (domainName?:'global')?.padRight(20) + "|" + secretId?.padRight(38)+" | " +username?.padRight(20)+" | " +password?.padRight(40) + " | " +description)
   }
+  
+  cred.with {
+    switch(it.class.name) {
+      case "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl":
+      showRow("user/password", domain, id, username, password?.plainText, description)
+      break
+        case "com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey":
+      showRow("ssh priv key", domain, id, privateKeySource?.privateKey?.plainText, passphrase?.decrypt()?:"", description)
+      break
+        case "com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl":
+      showRow("aws", domain, id, accessKey, secretKey?.plainText, description)
+      break
+        case "org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl":
+      showRow("secret text", domain, id, secret?.plainText, '', description)
+      break
+        case "org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl":
+      showRow("secret file", domain, id, content?.text, '', description)
+      break
+        case "com.microsoft.azure.util.AzureCredentials":
+      showRow("azur", domain, id, subscriptionId, "${clientId}:${hudson.util.Secret.decrypt(clientSecret)}", description)
+      break
+        case "org.jenkinsci.plugins.docker.commons.credentials.DockerServerCredentials":      
+      showRow("docker", domain, id, clientCertificate, clientKey, description)
+      break
+        case "com.dabsquared.gitlabjenkins.connection.GitLabApiTokenImpl":
+      showRow("gitlab", domain, id, apiToken?.plainText, '' , description)
+      break      
+        default:
+        showRow("something else", domain, id, it.class.name, '', description)
+    }         
+  }
+
 }
